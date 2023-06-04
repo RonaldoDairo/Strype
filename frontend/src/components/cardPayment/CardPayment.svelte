a7433bf987ff3ef3bde6b6e994f870b0b27a9aa9
RonaldoDairo <programaciondairo@gmail.com>
2023-06-04 14:55:49 +0200

    first commit

---
+++ frontend/src/components/cardPayment/CardPayment.svelte
@@ -0,0 +1,19 @@
+<script>
+import { loadStripe } from "@stripe/stripe-js"
+import {onMount} from "svelte";
+
+
+onMount(()=>{
+
+})
+function loadStripeElements(){
+loadStripe()
+}
+
+</script>
+
+<div>
+<h1>Prueba</h1>
+</div>
+<style>
+</style>
